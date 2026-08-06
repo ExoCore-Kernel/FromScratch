9f0d9c3a74fe5b13d383967b795bcc0ad887140e
ExoCore-Kernel <jacktp2013@gmail.com>
2026-08-06 15:35:23 +0800

    Load compact iPhone interface assets

--- public/coi-serviceworker.js
+++ public/coi-serviceworker.js
@@ -1,1 +1,21 @@
-(()=>{if(!window.isSecureContext||!navigator.serviceWorker||window.crossOriginIsolated)return;navigator.serviceWorker.register(new URL('coi-serviceworker.js',document.baseURI)).then(r=>{if(r.active&&!navigator.serviceWorker.controller)location.reload();}).catch(console.error);})();
+(() => {
+  const base = document.baseURI;
+
+  const mobileStyle = document.createElement('link');
+  mobileStyle.rel = 'stylesheet';
+  mobileStyle.href = new URL('mobile-interface.css?v=1', base).href;
+  document.head.append(mobileStyle);
+
+  const mobileScript = document.createElement('script');
+  mobileScript.src = new URL('mobile-interface.js?v=1', base).href;
+  mobileScript.defer = true;
+  document.head.append(mobileScript);
+
+  if (!window.isSecureContext || !navigator.serviceWorker || window.crossOriginIsolated) return;
+  navigator.serviceWorker
+    .register(new URL('coi-serviceworker.js', base))
+    .then((registration) => {
+      if (registration.active && !navigator.serviceWorker.controller) location.reload();
+    })
+    .catch(console.error);
+})();
